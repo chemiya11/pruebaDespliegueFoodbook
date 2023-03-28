2f1fbf98076fea0c36eb97ccfed14cd2b189d043
chemiya <jmlozanoolmedo@gmail.com>
2023-03-28 15:29:26 +0200

    puerto

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const DB_USER="root"
 const DB_HOST="localhost"
 const DB_PASSWORD="password"
 const DB_NAME="aplicacion_recetas"
-const DB_PORT="3306"
+const DB_PORT=3306
 
 module.exports={
     DB_USER,DB_PORT,DB_PASSWORD,DB_NAME,DB_HOST
