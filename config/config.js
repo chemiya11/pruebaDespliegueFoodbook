2a6cf303744b6fa6207592bb72a5715ba8c2d964
chemiya <jmlozanoolmedo@gmail.com>
2023-03-28 15:44:48 +0200

    datos railway

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const DB_USER="root"
-const DB_HOST="0.0.0.0"
-const DB_PASSWORD="password"
-const DB_NAME="aplicacion_recetas"
-const DB_PORT=3306
+const DB_HOST="containers-us-west-177.railway.app"
+const DB_PASSWORD="nOiJzuGXmtXIdzwMJVFj"
+const DB_NAME="railway"
+const DB_PORT=6954
 
 module.exports={
     DB_USER,DB_PORT,DB_PASSWORD,DB_NAME,DB_HOST
